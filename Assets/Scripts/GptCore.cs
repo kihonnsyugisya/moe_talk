ef340d5de79fe5c2844e84d2d0367ecbe4dc05c8
田辺 <katuobrothers@icloud.com>
2023-03-15 23:01:21 +0900

    feat: ファイル、シーンにモデルの作成

---
+++ Assets/Scripts/GptCore.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public static class GptCore 
+{
+    static ReactiveDictionary<EMOTIONS, int> emotionData = new ReactiveDictionary<EMOTIONS, int>();
+
+    static string ChatGPT(string question)
+    {
+        return "d";
+    }
+
+    static string Translate(string lang)
+    {
+        return "d";
+    }
+
+
+
+}
+
+public enum EMOTIONS
+{
+    HAPPY,LOVE,SAD,ANGRY,FEAR 
+}
