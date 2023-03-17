9a2d58bc9b45d226b5d03acdf59c493114f1ba66
DESKTOP-D21KG3K\tanab <tanabe@sh-com.jp>
2023-03-17 13:39:08 +0900

    feat: TalkViewファイル作成

---
+++ Assets/Scripts/View/TalkView.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public InputField chatWindow;
+    public GameObject chatPanel;
+    public Text life;
+    public GameObject adInducationPanel;
+    public Button talkButton;
+}
