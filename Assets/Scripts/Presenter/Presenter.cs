26fef87ffbff680cf708214b709fefb5a2c2bc0d
田辺 <katuobrothers@icloud.com>
2023-03-23 23:17:11 +0900

    fix: 返答がUIに反映されないバグを修正
    chatgptメソッドを非同期で呼び出していなかったので

--- Assets/Scripts/Presenter/Presenter.cs
+++ Assets/Scripts/Presenter/Presenter.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using Cysharp.Threading.Tasks;
 
 
 public class Presenter : MonoBehaviour
 {
     public TalkView talkView;
     public TalkModel talkModel;
+
     // Start is called before the first frame update
     void Start()
     {
         talkView.talkButton.OnClickAsObservable().Subscribe( _=> talkModel.ShowChatWindow(talkView.chatWindow)).AddTo(this);
-        talkView.chatWindow.onEndEdit.AsObservable().Subscribe(q=> talkModel.ShowAns(talkModel.gptInstance.ChatGPT(q).ToString(),talkView.chatPanel)).AddTo(this);
+        talkView.chatWindow.onEndEdit.AsObservable().Subscribe(async(q) => talkModel.ShowAns(await talkModel.gptInstance.ChatGPT(q), talkView.chatPanel) ).AddTo(this);
         talkView.nextButton.OnClickAsObservable().Subscribe(_=> talkModel.CloseAns(talkView.chatPanel)).AddTo(this);
 
     }
