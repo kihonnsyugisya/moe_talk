7ad3400953bb3bf4d00358d9c35ac1c64b988858
田辺 <katuobrothers@icloud.com>
2023-03-25 21:15:03 +0900

    feat: スクリプト作成、アタッチ

---
+++ Assets/Scripts/View/LogView.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class LogView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Button logButton;
+    public GameObject logPanel;
+    public GameObject userContentsPanel;
+    public TextMeshProUGUI userContentsText;
+    public GameObject aiContentsPanel;
+    public TextMeshProUGUI aiContentsText;
+}
+    
