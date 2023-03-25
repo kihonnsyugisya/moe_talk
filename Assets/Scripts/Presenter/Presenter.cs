baac0b9706d84e00ef3ca1ea1d48c1665e382322
田辺 <katuobrothers@icloud.com>
2023-03-25 15:36:44 +0900

    fix: 入力完了時に前回と同じ質問と空の質問の場合は質問を投げないように修正
    OnEndEditが予期せず呼ばれるので、その対策のため

--- Assets/Scripts/Presenter/Presenter.cs
+++ Assets/Scripts/Presenter/Presenter.cs
@@ -14,9 +14,17 @@ public class Presenter : MonoBehaviour
     void Start()
     {
         talkView.talkButton.OnClickAsObservable().Subscribe( _=> talkModel.ShowChatWindow(talkView.chatWindow)).AddTo(this);
-        talkView.chatWindow.onEndEdit.AsObservable().Subscribe(async(q) => talkModel.ShowAns(await talkModel.gptInstance.ChatGPT(q), talkView.chatPanel) ).AddTo(this);
+        talkView.chatWindow.onEndEdit
+            .AsObservable()
+            .Where(q => q != "")
+            .DistinctUntilChanged()
+            .Subscribe(async (q) =>{
+                talkModel.ShowAns(await talkModel.gptInstance.ChatGPT(q), talkView.chatPanel);
+                talkModel.CleanChatWindow(talkView.chatWindow);
+            }).AddTo(this);
         talkView.nextButton.OnClickAsObservable().Subscribe(_=> talkModel.CloseAns(talkView.chatPanel)).AddTo(this);
-
+        talkView.chatWindow.onSelect.AddListener(_ => talkModel.OnSelectChatWindow(talkView.talkButton));
+        talkView.chatWindow.onDeselect.AddListener(_=> talkModel.OnDeselectChatWindow(talkView.talkButton));
     }
 
     // Update is called once per frame
