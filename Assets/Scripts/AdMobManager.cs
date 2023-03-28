4520c66d524671e92ff8e15b2c8c5f46f7b89655
DESKTOP-D21KG3K\tanab <tanabe@sh-com.jp>
2023-03-28 10:25:27 +0900

    feat: テストリワード広告の実装

--- Assets/Scripts/AdMobManager.cs
+++ Assets/Scripts/AdMobManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
+using UniRx;
+
 
 public class AdMobManager : MonoBehaviour
 {
+    [HideInInspector] public BoolReactiveProperty isOnAdLoadedRewardedAd = new BoolReactiveProperty(false);
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +15,7 @@ public class AdMobManager : MonoBehaviour
         MobileAds.Initialize((initStatus) =>
         {
             // SDK initialization is complete
+            Debug.Log("admob init");
         });
     }
 
@@ -20,4 +24,26 @@ public class AdMobManager : MonoBehaviour
     {
         
     }
+
+    //動画広告同意パネルを表示する。(インスペクタでOnAdLoadedコールバックに設定)
+    public void OnRewardedAdLoaded()
+    {
+        isOnAdLoadedRewardedAd.Value = true;
+    }
+
+    //広告読み込みが失敗した時の処理。SDKからstringのパラメータを取得できる。(インスペクタでOnAdFailedToLoadコールバックに設定)
+    public void OnRewardAdFailedToLoad(string reason)
+    {
+        Debug.Log("リワード広告のロードに失敗 ><;" + reason);
+    }
+
+    // Rewarded Adオブジェクトの報酬獲得コールバックにセットする(インスペクタでOnUserEarnedRewardコールバックに設定)
+    // SDKからRewardクラスのパラメータを取得できる。
+    public void OnUserEarnedRewardAd(Reward reward)
+    {
+        Debug.Log("ユーザはリワードを取得しました。: リワードのタイプ=" +
+            reward.Type + ", リワードの量=" + reward.Amount);
+        isOnAdLoadedRewardedAd.Value = false;
+    }
+
 }
