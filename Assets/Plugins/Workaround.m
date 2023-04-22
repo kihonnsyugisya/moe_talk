a7d77f78be71978473378eff35210dde44274cee
kihonnsyugisya <74089076+kihonnsyugisya@users.noreply.github.com>
2023-04-22 23:39:22 +0900

    Revert "Feature/#41 update api network error"

--- Assets/Plugins/Workaround.m
+++
@@ -1,9 +0,0 @@
-typedef const void *GADUTypeInterstitialRef;
-
-typedef const void *GADUTypeRewardedAdRef;
-
-typedef const void *GADUTypeRequestRef;
-
-void GADURequestInterstitial(GADUTypeInterstitialRef interstitial, GADUTypeRequestRef request) { }
-
-void GADURequestRewardedAd(GADUTypeRewardedAdRef rewardedAd, GADUTypeRequestRef request) { }
